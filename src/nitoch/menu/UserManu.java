2885e0b4539a48498e5777c72504765244cf76cc
natai <ntg7creation@gmail.com>
2019-03-10 19:02:12 +0200

    new manu

--- src/nitoch/menu/UserManu.java
+++ src/nitoch/menu/UserManu.java
@@ -9,21 +9,27 @@ import java.util.Scanner;
 
 import nitoch.sql.SQLConnection;
 
-public class MasterMenu extends Menu {
+public class UserManu extends Menu {
+
+	String UserName = null;
+
+	public UserManu(String userName) {
+		UserName = userName;
+	}
 
 	@Override
 	public void run() {
 		Scanner MyScanner = new Scanner(System.in);
 		System.out.println(
-				"enter you requset:\n1. Get info on <user>\n2. Change into on <user>\n3. Add user\n4. remove user <user>");
+				"enter you requset:\n1. Get Myinfo \n2. Change name <New Name>\n3. Change FamilyName <New FamilyName>\n4. Change Id <new ID>\n5. Change UserName <New UserName>\n6. Change PassWord <new PassWord>");
 		int action = -1;
-		String name = "";
+		String newInfo = "";
 		while (action == -1) {
 			String input = MyScanner.nextLine();
 			try (Scanner temp = new Scanner(input)) {
 				action = temp.nextInt();
-				if (action != 3)
-					name = temp.next();
+				if (action != 1)
+					newInfo = temp.next();
 			} catch (NoSuchElementException e) {
 				System.out.println("ples enter correct input ex- 2 dani");
 				action = -1;
@@ -32,25 +38,31 @@ public class MasterMenu extends Menu {
 
 		switch (action) {
 		case 1:
-			PrintUserInfo(name);
+			PrintUserInfo();
 			break;
 		case 2:
-			ChangeUserInfo(name);
+			ChangeUserName(newInfo);
 			break;
 		case 3:
-			AddUser();
+			ChangeUserFamilyName(newInfo);
 			break;
 		case 4:
-			RemoveUser(name);
+			ChangeUserUserName(newInfo);
+			break;
+		case 5:
+			ChangeId(newInfo);
+			break;
+		case 6:
+			ChangePassword(newInfo);
 			break;
 		default:
 			break;
 		}
 	}
 
-	private void PrintUserInfo(String user) {
+	private void PrintUserInfo() {
 		SQLConnection sql = new SQLConnection();
-		String SQlCommand = "SELECT * from Users where UserName = '" + user + "';";
+		String SQlCommand = "SELECT * from Users where UserName = '" + UserName + "';";
 
 		try (Connection conn = sql.connect(); Statement stmt = conn.createStatement();) {
 			// output = stmt.executeQuery(SQlCommand);
@@ -63,15 +75,23 @@ public class MasterMenu extends Menu {
 		}
 	}
 
-	private void ChangeUserInfo(String user) {
+	private void ChangeUserName(String NewName) {
 
 	}
 
-	private void AddUser() {
+	private void ChangeUserFamilyName(String NewUserName) {
 
 	}
 
-	private void RemoveUser(String user) {
+	private void ChangeUserUserName(String NewUserName) {
+
+	}
+
+	private void ChangeId(String ID) {
+
+	}
+
+	private void ChangePassword(String NewPassword) {
 
 	}
 }
