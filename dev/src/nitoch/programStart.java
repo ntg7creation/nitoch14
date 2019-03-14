5495b26ef9f0f94ee032250671b3783213ad6046
natai <ntg7creation@gmail.com>
2019-03-14 19:09:37 +0200

    tt

---
+++ dev/src/nitoch/programStart.java
@@ -0,0 +1,15 @@
+package nitoch;
+
+import nitoch.menu.UserMenu;
+
+public class programStart {
+
+	public static void main(String args[])
+	{
+		//MasterMenu Mm = new MasterMenu();
+		//Mm.run();
+		UserMenu UM = new UserMenu("ntg");
+		UM.run();
+		//chang
+	}
+}
